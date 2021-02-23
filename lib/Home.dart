025872f0c94184726a6244a2db4d6472b6cf9cb5
ahmer7aa <ahmer5253@gmail.com>
2021-02-23 11:12:38 +0500

    Complete Simple Blog App

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,3 +1,4 @@
+import 'package:blog_app/PostDetailsPage.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:async';
@@ -13,6 +14,7 @@ class _HomeState extends State<Home> {
   List<DocumentSnapshot> snapshot;
   CollectionReference collectionReference =
       FirebaseFirestore.instance.collection('BlogPost');
+
   @override
   void initState() {
     super.initState();
@@ -21,6 +23,12 @@ class _HomeState extends State<Home> {
         snapshot = dataSnapshot.docs;
       });
     });
+  }
+
+  // TODO Pass Data Method
+  passData(DocumentSnapshot snap) {
+    Navigator.of(context).push(
+        MaterialPageRoute(builder: (context) => PostDetails(snapshot: snap)));
   }
 
   @override
@@ -45,7 +53,8 @@ class _HomeState extends State<Home> {
           children: [
             UserAccountsDrawerHeader(
               decoration: BoxDecoration(
-                gradient: LinearGradient(colors: [Colors.purple, Colors.red]),
+                gradient: LinearGradient(
+                    colors: [Colors.purpleAccent, Colors.redAccent]),
               ),
               accountName: Text('Ahmer Iqbal'),
               accountEmail: Text('ahmer5253@gmail.com'),
@@ -70,14 +79,6 @@ class _HomeState extends State<Home> {
               leading: Icon(Icons.menu, color: Colors.green),
               title: Text('Fourth Page'),
             ),
-            ListTile(
-              leading: Icon(Icons.cake, color: Colors.purple),
-              title: Text('First Page'),
-            ),
-            ListTile(
-              leading: Icon(Icons.cake, color: Colors.purple),
-              title: Text('First Page'),
-            ),
             Divider(height: 10.0, color: Colors.black),
             ListTile(
               trailing: Icon(Icons.close, color: Colors.red),
@@ -89,6 +90,61 @@ class _HomeState extends State<Home> {
           ],
         ),
       ),
+      body: snapshot == null
+          ? Container(
+              alignment: Alignment.center,
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: snapshot.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  elevation: 10.0,
+                  margin: EdgeInsets.all(10.0),
+                  child: Container(
+                    padding: EdgeInsets.all(10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        CircleAvatar(
+                          child: Text(snapshot[index].data()['title'][0]),
+                          backgroundColor: Colors.redAccent,
+                          foregroundColor: Colors.white,
+                        ),
+                        SizedBox(width: 10.0),
+                        Container(
+                          width: 210.0,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              InkWell(
+                                child: Text(
+                                  snapshot[index].data()['title'],
+                                  maxLines: 1,
+                                  style: TextStyle(
+                                    fontSize: 22.0,
+                                    color: Colors.green,
+                                  ),
+                                ),
+                                onTap: () {
+                                  // pass each and every index data
+                                  passData(snapshot[index]);
+                                },
+                              ),
+                              SizedBox(height: 5.0),
+                              Text(
+                                snapshot[index].data()['content'],
+                                maxLines: 2,
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
     );
   }
 }
